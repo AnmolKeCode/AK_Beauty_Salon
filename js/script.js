85f736c8098aa5aeb3ff740c1cf24e19ac34afd3
AnmolKeCode <chanmolsingh15@gmail.com>
2023-08-04 16:21:50 +0530

    second commit

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@
 
 let menuToggle = document.querySelector('.menuToggle');
 let header = document.querySelector('header');
-let section = document.querySelector('section');
+// let section = document.querySelector('section');
+let section = document.getElementsByTagName('*');
 
-menuToggle.onclick = function(){
+
+menuToggle.onclick = function () {
     header.classList.toggle('active')
     section.classList.toggle('active')
 }
