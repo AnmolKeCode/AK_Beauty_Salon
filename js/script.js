aac8139c8636cefa9202366b1a721f8856e3e16b
AnmolKeCode <chanmolsingh15@gmail.com>
2023-08-07 22:45:45 +0530

    Six Commit

--- js/script.js
+++ js/script.js
@@ -2,11 +2,12 @@
 
 let menuToggle = document.querySelector('.menuToggle');
 let header = document.querySelector('header');
-// let section = document.querySelector('section');
-let section = document.getElementsByTagName('*');
+let section = document.querySelector('section');
+// let section = document.getElementsByTagName('*');
 
 
 menuToggle.onclick = function () {
     header.classList.toggle('active')
     section.classList.toggle('active')
 }
+
